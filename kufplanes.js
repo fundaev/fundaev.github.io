d81d88a0ab93a2ee19dd1196622ce2a837164e37
Sergei Fundaev <fundaev@satracks.org>
2017-03-23 19:23:28 +0400

    txt -> json

---
+++ kufplanes.js
@@ -0,0 +1,29 @@
+{
+    "Аэрофлот РА": {
+        "A-319": "https://cdn.jetphotos.com/full/6/72648_1485888954.jpg",
+        "A-320": "https://cdn.jetphotos.com/full/6/47425_1488662532.jpg",
+        "A-321": "https://cdn.jetphotos.com/full/6/60819_1489226318.jpg",
+        "A-330": "https://cdn.jetphotos.com/full/6/23168_1488093207.jpg",
+        "B-737-8": "https://cdn.jetphotos.com/full/5/42876_1488984424.jpg",
+        "B-767": "https://cdn.jetphotos.com/full/3/45214_1355278129.jpg",
+        "B-777": "https://cdn.jetphotos.com/full/6/69912_1486023778.jpg"
+    },
+    "Сибирь": {
+        "A-319": "https://cdn.jetphotos.com/full/6/17218_1443373767.jpg",
+        "A-320": "https://d31asmy75eposw.cloudfront.net/full/6/82747_1453814471.jpg",
+        "A-321": "https://cdn.jetphotos.com/full/6/20344_1489397338.jpg",
+        "B-737-8": "https://cdn.jetphotos.com/full/6/20284_1471696989.jpg"
+    },
+    "ЮТэйр": {
+        "B-737": "https://d31asmy75eposw.cloudfront.net/full/6/45236_1465980581.jpg"
+    },
+    "Оренбуржье": {
+        "Л-410": "http://cdn-www.airliners.net/photos/airliners/9/3/9/2318939.jpg?v=v40"
+    },
+    "Победа": {
+        "B-737-800": "https://d31asmy75eposw.cloudfront.net/full/6/52958_1456698403.jpg"
+    },
+    "РедВингс": {
+        "B-757": "https://d31asmy75eposw.cloudfront.net/full/5/24781_1479390950.jpg"
+    }
+}
